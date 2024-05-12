01a49c5439d00c8a5f80a82bdc4c5ca7cb023c65
Afreed <afreed178@gmail.com>
2024-05-12 18:41:53 +0530

    added docker to platform service

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -13,15 +13,13 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseRouting();
+app.MapGet("/", () => "Hello World!");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 app.MapControllers();
 
-//app.UseHttpsRedirection();
-
 PrepDb.PrepPopulation(app);
+
 app.Run();
