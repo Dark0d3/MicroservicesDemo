63bbe3a9496843c441831b522294587bf4ad910e
Afreed <afreed178@gmail.com>
2024-05-12 19:11:24 +0530

    added K8S

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fcc5d88164753ed70262cff7704521a41161a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b3f6399f6c949a43067ab5732f7b644df75936")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MicroservicesDemo
+
 Learning Microservices => .net, RabbitMQ, Docker so on
+
+Docker Commands
+
+docker ps
+docker build -t dark0d3/platformservice .
+docker run --name platformservice -p 8080:8080 -d dark0d3/platformservice
+docker stop <container-id>
+docker start <container-id>
+docker inspect <container-id>
+docker logs <container-id>
+
+K8S Commands
+
+kubectl apply -f platforms-depl.yaml
+kubectl get deployments
+kubectl get pods
+kubectl get services
+
+delete deployment
+kubectl delete deployment platforms-depl --that is the deployment name
